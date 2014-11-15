cecb0593b3327bee2aaf95e2fc9be8f2f48a1225
Nate Weiner <nateweiner@gmail.com>
2014-11-14 22:19:16 -0500

    brain tree first pass

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'puma'
 gem 'pundit'
 gem 'simple_form'
 gem 'upmin-admin'
+gem 'braintree'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment  = :sandbox        
+Braintree::Configuration.merchant_id   = "5wzrck8bsvmmdvxg"
+Braintree::Configuration.public_key   = "82crnd5jfrcbnvr7"
+Braintree::Configuration.private_key  = "6025dc81c2a9f0086352122d04d6baac"
