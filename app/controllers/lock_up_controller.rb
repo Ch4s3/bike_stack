6a6d170db443469a466b410b438732716464f11e
Chase <chase.gilliam@gmail.com>
2014-11-14 21:06:29 -0500

    added basic lockups

---
+++ app/controllers/lock_up_controller.rb
@@ -0,0 +1,30 @@
+class LockUpController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def submit
+    if params.present?
+      name = params["name"]
+      lat = params["lat"]
+      lon = params["lon"]
+
+      LockUp.create(name: name, lat: lat, lon: lon)
+    end
+  end
+
+  def index
+    @spots = LockUp.all
+  end
+
+  def spots
+    @spots = LockUp.all
+    respond_to do |format|
+      format.html  
+      format.json  { render json: @spots }
+    end
+
+  end
+end
---
+++ spec/factories/lock_ups.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :lock_up do
+    lat 1.5
+lon 1.5
+name "MyString"
+description "MyText"
+  end
+
+end
