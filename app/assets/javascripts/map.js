999a7906731e0d7ff18ee46612e1eb28575f2b7e
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-11-16 12:14:47 -0500

    deleted duplicate file from map partial and added span tags to popup for cleaner look

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     var totalVotes = lockup["total_votes"];
     var marker = L.marker([lat, lon]).addTo(map);
     marker._leaflet_id = cid;
-    marker.bindPopup( name + "<br />" + description + "<button class='upvote vote' data-up-id='"+cid+"'>UpVote</button><p class='total-votes'>"+totalVotes+"</p><button class='downvote vote' data-down-id='"+cid+"'>DownVote</button>" );
+    marker.bindPopup( "<span>"+ name + "</span><br /><span>" + description + "</span><br /><button class='upvote vote' data-up-id='"+cid+"'>UpVote</button><span class='total-votes'>"+totalVotes+"</span><button class='downvote vote' data-down-id='"+cid+"'>DownVote</button>" );
     markerArray[cid] = marker;
     gonArray[cid] = lockup;
   });
@@ -38,16 +38,6 @@ $(document).ready(function() {
     }
   });
 
-  // map.on("popupclose", function(e) {
-  //   var marker = e.popup._source;
-  //   debugger;
-  //   // marker.bindPopup( name + "<br />" + description + "<button class='upvote vote' data-up-id='"+cid+"'>UpVote</button><p class='total-votes'>"+totalVotes+"</p><button class='downvote vote' data-down-id='"+cid+"'>DownVote</button>" );
-  //   var cid = marker._leaflet_id
-  //   var description =
-  //   var totalVotes =
-  //   e.setContent( name + "<br />" + description + "<button class='upvote vote' data-up-id='"+cid+"'>UpVote</button><p class='total-votes'>"+totalVotes+"</p><button class='downvote vote' data-down-id='"+cid+"'>DownVote</button>" );
-  // });
-
   function ajaxVote(id, direction) {
     $.ajax({
       url: "/lock_ups/vote",
@@ -56,13 +46,12 @@ $(document).ready(function() {
       data: { vote: { lock_up_id: id, direction: direction}},
       success: function(response) {
         // debugger;
-        console.log(response);
         $(".total-votes").html(response);
         var marker = markerArray[id];
         var lockup = gonArray[id];
-        var name = lockup["name"]
-        var description = lockup["description"]
-        marker.bindPopup( name + "<br />" + description + "<button class='upvote vote' data-up-id='"+id+"'>UpVote</button><p class='total-votes'>"+response+"</p><button class='downvote vote' data-down-id='"+id+"'>DownVote</button>" );
+        var name = lockup["name"];
+        var description = lockup["description"];
+        marker.bindPopup( "<span>"+ name + "</span><br /><span>" + description + "</span><br /><button class='upvote vote' data-up-id='"+id+"'>UpVote</button><span class='total-votes'>"+response+"</span><button class='downvote vote' data-down-id='"+id+"'>DownVote</button>" );
       }
     })
   }
